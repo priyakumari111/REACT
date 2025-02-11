35c01b1211def7a327dd5073466aead478556296
Priya Kumari <157005951+priyakumari111@users.noreply.github.com>
2025-02-11 13:20:16 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,67 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { SnackbarProvider } from "notistack";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@mui/system";
+import theme from "./theme";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <ThemeProvider theme={theme}>
+        <SnackbarProvider
+          maxSnack={1}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "center",
+          }}
+          preventDuplicate
+        >
+          <App />
+        </SnackbarProvider>
+        </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+   document.getElementById('root')
+);
+
+****************************************************************************
+  index.css
+
+@import url("https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap");
+
+body {
+  margin: 0;
+  font-family: "Lato", -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
+    "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+    monospace;
+}
+*************************************************************************
+ theme.js
+  
+  import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Lato"
+  },
+  palette: {
+    primary: {
+      light: "#45c09f",
+      main: "#00a278",
+      dark: "#00845c",
+      contrastText: "#fff",
+    },
+  },
+});
+
+export default theme;
